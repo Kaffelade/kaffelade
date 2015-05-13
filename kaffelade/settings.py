26d14caea48e86a7a24842331bc5edb7b7202591
Casper B. Hansen <fvx507@alumni.ku.dk>
2015-05-13 17:46:32 +0200

    Added blog and base apps, as well as some initial templates and style.

--- kaffelade/settings.py
+++ kaffelade/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'kaffelade.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,3 +100,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    '/var/www/static/',
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
